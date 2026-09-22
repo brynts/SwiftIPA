86bddf5373351acef7c82203821c1696674f24bf
xsxs18 <kaan97431@gmail.com>
2026-09-22 21:02:19 +0200

    Add the missing openssl/x509v3.h include for the self-signed identity generator

--- SwiftIPA/Signing/ZSign/ZSignBridge.mm
+++ SwiftIPA/Signing/ZSign/ZSignBridge.mm
@@ -11,6 +11,9 @@
 #include <openssl/pkcs12.h>
 #include <openssl/rsa.h>
 #include <openssl/evp.h>
+#include <openssl/x509v3.h>
+#include <openssl/asn1.h>
+#include <openssl/bio.h>
 #include <time.h>
 #endif
 
